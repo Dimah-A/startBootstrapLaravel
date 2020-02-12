6d4178e3e48b0bf4ef22bb5a24a6f8cb863bd808
assma <dimah.assma@gmail.com>
2020-02-12 16:29:48 +0100

    debut

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+require('./bootstrap');
+require('bootstrap');
+require('jquery')
+require('jquery.easing/jquery.easing')
+require('./jqBootstrapValidation')
+require('./contact_me')
+
+require('./agency');
+
